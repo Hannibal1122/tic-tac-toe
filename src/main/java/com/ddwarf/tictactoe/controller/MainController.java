56999a1ff16ceddf9df4d92ba6aa526b8ec77d68
Mikhail <mwork92@gmail.com>
2022-03-24 22:31:49 +0300

    init

---
+++ src/main/java/com/ddwarf/tictactoe/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.ddwarf.tictactoe.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/main")
+    public String main(@RequestParam(name = "name", required = false, defaultValue = "Wl") String name, Model model) {
+        model.addAttribute("name", name);
+        return "main";
+    }
+}
