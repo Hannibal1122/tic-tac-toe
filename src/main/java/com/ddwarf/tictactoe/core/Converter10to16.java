e2677d8045d39429576324b6750a354029ef7ea7
coffeendell13 <102995244+coffeendell13@users.noreply.github.com>
2022-04-14 16:48:26 +0300

    доработан класс конвертер10в16

--- src/main/java/com/ddwarf/tictactoe/core/Converter10to16.java
+++ src/main/java/com/ddwarf/tictactoe/core/Converter10to16.java
@@ -4,17 +4,18 @@ import java.sql.SQLOutput;
 
 public class Converter10to16 {
     public static char[] I = {'A', 'B', 'C', 'D', 'E', 'F'};
-    public static String to16 (int x) {
+    public static String to16 (int x, int n) {
         int z;
         String y = "";
-        while (x >= 16) {
-            z = x % 16;
+        while (x >= n) {
+            z = x % n;
             y = y + Converter10to16.toChar(z);
-            x = x/16;
+            x = x/n;
         }
         y = y + Converter10to16.toChar(x);
         return new StringBuilder(y).reverse().toString();
     }
+    // код не будет работать с 2 и 8-ричными системами из-за метода toChar
     private static String toChar (int x) {
         if (x < 10) {
             return String.valueOf(x);
