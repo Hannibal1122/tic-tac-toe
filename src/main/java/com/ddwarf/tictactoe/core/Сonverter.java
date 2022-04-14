372570006b3620a7311b24fa8584bdc1a09cd6d7
coffeendell13 <102995244+coffeendell13@users.noreply.github.com>
2022-04-14 15:35:59 +0300

    добавлен конвертер из 2 в 16

--- src/main/java/com/ddwarf/tictactoe/core/Сonverter.java
+++ src/main/java/com/ddwarf/tictactoe/core/Сonverter.java
@@ -1,46 +1,20 @@
-/*
+
 package com.ddwarf.tictactoe.core;
-
-import java.util.Scanner;
-
 public class Сonverter {
 
-    public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
-        String binaryNumber = console.nextLine();
-        System.out.println("Двоичное число " + binaryNumber + " равно шестнадцатеричному числу " + toHex(binaryNumber));
-        String hexNumber = console.nextLine();
-        System.out.println("Шестнадцатеричное число " + hexNumber + " равно двоичному числу " + toBinary(hexNumber));
-    }
-
-    public static String toHex(String binaryNumber) {
-
-        if ((binaryNumber == null) || (binaryNumber == "")){
-            return "";
-        }
-        else if (binaryNumber ) { //HELP!!! нужно прописать условие: если в строке содержится любой символ кроме 0 и 1
-            return "";
-        }
-
-        if (!(binaryNumber.length()%4 == 0)) {
-            while (!(binaryNumber.length()%4 == 0)) {
-                binaryNumber = "0" + binaryNumber;
-            }
-        }
-        else {
-            binaryNumber = binaryNumber.substring(4); //нужно разбить строку по 4 символа
-        }
-
-    }
-
-    public static String toBinary(String hexNumber) {
-
-        if ((hexNumber == null) || (hexNumber == "")) {
-            return "";
-        }
-        else if ()
-
-    }
-
+     public static String in16 (String x) {
+         int b = 0; // в эту переменную записываем десятичное число
+         /* вариант 1
+         for (int i = 0; i < x.length(); i++) {
+             b <<= 1;
+             if (x.charAt(i) == '1') b++;
+         } */
+         // вариант 2
+         for (int i = 0; i < x.length(); i++) { //когда i меньше длины двоичного числа
+             b = (int) (b + x.charAt(i) * Math.pow(2, i)); /*в десятичное записываем
+             предыдущее b плюс i-тая цифра в двоичном числе умноженная на 2 в степени i
+             (int) - обозначает преобразование типа String к типу int */
+         }
+         return Converter10to16.to16(b);
+     }
 }
-*/
