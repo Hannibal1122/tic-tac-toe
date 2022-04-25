bbc1a4f6548f818cb70f0af8af95f6fa0294df5d
coffeendell13 <102995244+coffeendell13@users.noreply.github.com>
2022-04-25 12:23:19 +0300

    добавлен класс с бесконечным циклом

---
+++ src/main/java/com/ddwarf/tictactoe/core/NumberG.java
@@ -0,0 +1,17 @@
+package com.ddwarf.tictactoe.core;
+
+public class NumberG {
+
+    public static boolean a() {
+        for (long b = 2; b <= 2147000000 ; b++) {
+            while (b != 1) {
+                if (b % 2 == 0) {
+                    b = b / 2;
+                } else {
+                    b = 3 * b + 1;
+                }
+            }
+        }
+        return true;
+    }
+}
