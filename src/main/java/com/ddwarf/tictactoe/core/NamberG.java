25b67479aef64121a8662c594288d68643ac1c9b
Chemichanga7 <anwork05@gmail.com>
2022-05-24 22:19:37 +0300

    I'm back!

---
+++ src/main/java/com/ddwarf/tictactoe/core/NamberG.java
@@ -0,0 +1,16 @@
+package com.ddwarf.tictactoe.core;
+
+public class NamberG {
+    public static boolean a() {
+        for (long b = 2; b <= 2147000000 ; b++) {
+            while (b != 1) {
+                if (b % 2 == 0) {
+                    b = b / 2;
+                } else {
+                    b = 3 * b + 1;
+                }
+            }
+        }
+        return true;
+    }
+}
