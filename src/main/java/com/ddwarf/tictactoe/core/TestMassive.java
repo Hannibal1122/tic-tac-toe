dfd165d5b0168fe151e5f69a9cc404fedd345f50
Mikhail <mwork92@gmail.com>
2022-06-05 10:31:35 +0300

    any

--- src/main/java/com/ddwarf/tictactoe/core/TestMassive.java
+++ src/main/java/com/ddwarf/tictactoe/core/TestMassive.java
@@ -1,17 +1,27 @@
 package com.ddwarf.tictactoe.core;
 
-public abstract class TestMassive {
+public class TestMassive extends MassiveAbstract {
+    public void swap(int h) {
+        int b = neon[h];
+        neon[h] = neon[h + 1];
+        neon[h + 1] = b;
+    }
+}
+interface MassiveInterface
+{
+    public void TestMassive(int x);
+    public void swap(int h);
+}
+
+abstract class MassiveAbstract implements MassiveInterface
+{
     int[] neon;
 
-    public void TestMassive(int x) {
+    public void TestMassive(int x)
+    {
         neon = new int[x];
         for (int i = 0; i < x; ++i) {
             neon[i] = i;
         }
-    }
-    public void svap(int h) {
-        int b = neon[h];
-        neon[h] = neon[h + 1];
-        neon[h + 1] = b;
     }
 }
