f3098ebe1687de02f03fa680989e9c507d7446c0
Chemichanga7 <anwork05@gmail.com>
2022-06-26 16:56:27 +0300

    Наработки по Альтрону

--- src/main/java/com/ddwarf/tictactoe/core/ttt/ai/Altron.java
+++ src/main/java/com/ddwarf/tictactoe/core/ttt/ai/Altron.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 
 import com.ddwarf.tictactoe.core.ttt.classes.ClickByFieldEmit;
 
+
+
 public class Altron extends MainAI {
     Logger logger = LoggerFactory.getLogger("Tic-Tac-Toe");
 
@@ -15,8 +17,23 @@ public class Altron extends MainAI {
         this.field = field;
     }
     public ClickByFieldEmit getNextClick() {
-        // TODO Auto-generated method stub
+        MinMax();
         return null;
+    }
+    // метод функции минмакс
+    public int MinMax()
+    {
+        //превод информации из двумерного массива в одномерный
+        int[] newBoard = new int[field.length * field[0].length];
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[0].length; j++) {
+                newBoard[i * field[0].length + j] = field[i][j];
+            }
+        }
+        return 0;
     }
 
 }
+
+//[0]        [1]        [2]
+//[1][2][3]  [1][2][3]  [1][2][3]
